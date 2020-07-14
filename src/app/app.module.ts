5b137124949726682b21d6db84d5b5a467d81600
Racem Cherni <racem.cherni@esprit.tn>
2020-07-14 16:28:12 +0100

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
+
 
 
 
@@ -45,7 +47,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -12,5 +12,17 @@ export class DishService {
     return DISHES;
   }
 
+
+
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0] ;
+
+  }
+
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
+  }
+
   constructor() { }
 }
