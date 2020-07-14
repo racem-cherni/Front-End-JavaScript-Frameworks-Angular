6841ba122eb936124c1c80c5f4497f91bc5d02cb
Racem Cherni <racem.cherni@esprit.tn>
2020-07-14 01:49:43 +0100

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,15 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,7 @@ import { MenuComponent } from './menu/menu.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
